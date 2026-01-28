6c627538d2bccfac16e8cb79c9427e3f85c62804
elyxiaaa <oquinianoa@gmail.com>
2026-01-28 13:30:25 +0800

    Test fixing vercel build

---
+++ src/pages/subpages/TypeCArmor.js
@@ -0,0 +1,5 @@
+import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
+function TypeCArmor() {
+    return (_jsxs("div", { className: "min-h-screen bg-gray-900 text-white flex flex-col items-center px-4 py-12", children: [_jsx("h2", { className: "text-4xl font-extrabold mb-6", children: "Type C Armor Crafting" }), _jsx("p", { className: "text-xl", children: "Here is the detailed information about Type C Armor Crafting..." })] }));
+}
+export default TypeCArmor;
