ede16ea53bab81e9fc878b7a9f2cc65856516fec
elyxiaaa <oquinianoa@gmail.com>
2026-01-28 15:17:04 +0800

    Try fixing deployment

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import { jsx as _jsx, jsxs as _jsxs } from "react/jsx-runtime";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from './pages/Home';
-import Combination from './pages/Combination';
-import TypeCArmor from './pages/subpages/TypeCArmor';
-function App() {
-    return (_jsx(BrowserRouter, { children: _jsxs(Routes, { children: [_jsx(Route, { path: "/", element: _jsx(Home, {}) }), _jsx(Route, { path: "/Combination", element: _jsx(Combination, {}) }), _jsx(Route, { path: "/Combination/TypeCArmor", element: _jsx(TypeCArmor, {}) })] }) }));
-}
-export default App;
