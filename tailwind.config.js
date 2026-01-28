0a7d4e1cf25326d0f2bc7dcdc850ae69dba034b9
elyxiaaa <oquinianoa@gmail.com>
2026-01-28 16:15:05 +0800

    Test Fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,14 +8,13 @@ export default {
   ],
   theme: {
     extend: {
-       backgroundImage: {
-  'BGHome': "url('/public/BGHome.jpg')",
-  'BGCombination': "url('/public/BGCombination.jpg')",
-  'BGGuilds': "url('/src/assets/BGGuilds.jpg')",
-  'BGLong': "url('/public/BGLong.png')",
-},
+      backgroundImage: {
+        'BGHome': "url('/public/BGHome.jpg')",
+        'BGCombination': "url('/public/BGCombination.jpg')",
+        'BGGuilds': "url('/src/assets/BGGuilds.jpg')",
+        'BGLong': "url('/public/BGLong.png')",
+      },
     },
   },
   plugins: [require('daisyui')],
 }
-
